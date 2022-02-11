3cff98a3d0ccfded6cbf6b869e230182fd9650eb
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-02-11 10:42:54 +0200

    Progress

--- main.cc
+++ main.cc
@@ -1,13 +1,9 @@
 #include <drogon/drogon.h>
 
 int main() {
-    //Set HTTP listener address and port
     drogon::app()
             .addListener("0.0.0.0", 8888)
-            .setThreadNum(16);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
+            .setThreadNum(16)
+            .run();
     return 0;
 }
